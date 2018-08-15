e0d0fcc16e708cd9e6d651798fb5f0929c7bbdc8
Joseph Plaugher <joseph@localhost.localdomain>
2018-08-14 21:40:31 -0700

    routing works well except if a bad parameter is set. Need to convert protype to controll the flow better

--- src/model/blog.js
+++ src/model/blog.js
@@ -1,15 +1,90 @@
-const Query = require('./../../util/Query');
+const Conn = require('./../util/postgres');
 
-const GetBlogList = (req, res) => {
-    let q = "SELECT postid, title, description, body, published FROM posts WHERE published = 'true' ORDER BY postid DESC"
-    //const FetchBlogList = new Query(q);
-    try {
-      var result = findInvoiceQuery.runQuery(q);
-    } catch (er) {
-      console.log('sql error', er);
-    }
-
-    var bloglist = result.map( (row) => {
-       // "<div>" {row.id} " "{row.title} " "{row.description} " "{row.body} "</div>";
+const getBlogList = (req, res, next) => {
+  Conn.query(`
+    SELECT title, description, postid 
+    FROM posts 
+    ORDER BY postid DESC
+  `)
+    .then(resp => {
+      let list = '<div>';
+      let data = resp.rows;
+      data.forEach(elem => {
+        list += '<div id="' + elem.postid +'">' + elem.title + '<br/>' + elem.description + '</div>';
+      });
+      list += '</div>';
+      req.bloglist = data;
+      req.markedUplist = list;
+      next();
     })
 }
+
+const getCurrentPost = (req, res, next) => {
+  var query;
+  if(req.params.postid) { 
+    query = {
+      "text":`
+        SELECT title, description, body, postid 
+        FROM posts 
+        WHERE postid=$1 `,
+      "values":[req.params.postid]}
+    }else{
+    query = `
+      SELECT title, description, body, postid 
+      FROM posts  
+      ORDER BY postid DESC LIMIT 1`;
+      console.log('which one: 2');
+    }
+  Conn.query(query)
+    .then(resp => {
+      req.latest = resp.rows;
+      next();
+    }) 
+}
+
+const getPostByID = (req, res, next) => {
+  console.log('the requested post: ', req.params.postid);
+  var query = {
+    "text":`
+      SELECT title, description, body, postid 
+      FROM posts 
+      WHERE postid=$1 
+      ORDER BY postid DESC`,
+    "values":[req.params.postid]}
+  Conn.query(query)
+    .then(resp => {
+      req.latest = resp.rows;
+      next();
+    }) 
+}
+
+const getComments = (req, res, next) => {
+  var query = {
+    "text":`
+      SELECT commentid, 
+          username, 
+          body 
+      FROM comments
+      WHERE postid=$1 
+      ORDER BY postid DESC`,
+    "values":[req.params.postid]}
+  Conn.query(query)
+    .then(resp => {
+      let comm = '<div>';
+      let data = resp.rows;
+      data.forEach(elem => {
+        comm += '<div id="' + elem.commentid +'">' + elem.username + '<br/>' + elem.body + '</div>';
+      });
+      comm += '</div>';
+      req.comments = comm;
+      res.render('index', {
+        title: req.latest[0].title,
+        description: req.latest[0].description,
+        body: req.latest[0].body,
+        blogList: req.markedUplist,
+        comments: req.comments
+      });
+    })
+}
+
+module.exports = {getBlogList, getCurrentPost, getPostByID, getComments};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-//const Query = require('./util/Query');
-const Conn = require('./util/postgres');
+const blog = require('./model/blog')
+const getBlogList = blog.getBlogList;
+const getLatestPost = blog.getLatestPost;
+const getCurrentPost = blog.getCurrentPost;
+const getComments = blog.getComments;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -19,67 +22,5 @@ app.use((req, res, next) => {
   next();
 });
 
-const getBlogList = (req, res, next) => {
-  Conn.query(`
-    SELECT title, description, postid 
-    FROM posts 
-    ORDER BY postid DESC
-  `)
-    .then(resp => {
-      let list = '<div>';
-      let data = resp.rows;
-      data.forEach(elem => {
-        list += '<div id="' + elem.postid +'">' + elem.title + '<br/>' + elem.description + '</div>';
-      });
-      list += '</div>';
-      req.bloglist = data;
-      req.markedUplist = list;
-      next();
-    })
-}
-
-const getLatestPost = (req, res, next) => {
-  var query = {
-    "text":`
-      SELECT title, description, body, postid 
-      FROM posts 
-      WHERE postid=$1 
-      ORDER BY postid DESC`,
-    "values":[req.bloglist[0].postid]}
-  Conn.query(query)
-    .then(resp => {
-      req.latest = resp.rows;
-      next();
-    }) 
-}
-
-const getComments = (req, res, next) => {
-  var query = {
-    "text":`
-      SELECT commentid, 
-          username, 
-          body 
-      FROM comments
-      WHERE postid=$1 
-      ORDER BY postid DESC`,
-    "values":[req.bloglist[0].postid]}
-  Conn.query(query)
-    .then(resp => {
-      let comm = '<div>';
-      let data = resp.rows;
-      data.forEach(elem => {
-        comm += '<div id="' + elem.commentid +'">' + elem.username + '<br/>' + elem.body + '</div>';
-      });
-      comm += '</div>';
-      req.comments = comm;
-      res.render('index', {
-        title: req.latest[0].title,
-        description: req.latest[0].description,
-        body: req.latest[0].body,
-        blogList: req.markedUplist,
-        comments: req.comments
-      });
-    })
-}
-
-app.get('/', getBlogList, getLatestPost, getComments);
+app.get('/', getBlogList, getCurrentPost, getComments);
+app.get('/postid/:postid', getBlogList, getCurrentPost, getComments);
