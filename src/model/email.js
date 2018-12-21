994a990559aa816faf300d77e09ff068de7e9c2f
Joseph <joseph@appreciateco.com>
2018-12-20 16:24:29 -0800

    emails are coming from thinkfree.life@gmail.com. Now I need to correct the double user entry issue

--- src/model/email.js
+++ src/model/email.js
@@ -6,26 +6,29 @@ const email = (user, req, res) => {
         service: 'gmail',
         secure: false, // true for 465, false for other ports
         auth: {
-            user: 'joseph@appreciateco.com', // generated ethereal user
-            pass: 'Edison1985!' // generated ethereal password
+            user: 'thinkfree.life@gmail.com', 
+            pass: 'Edison1985!'
         }
     });
-
+    let body = 'Hello ' + user.username + `,<br/><br/>
+    Thank you for reading! I hope you'll join in the discussion.<br/><br/>
+    If you want to subscribe to receive emails about new blog posts, just sign in and go to the user menu and click "Subscribe"<br/><br/>
+    Friedman Smith`;
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Joseph Plaugher" <joseph@appreciateco.com>', // sender address
+        from: '"Friedman Smith" <thinkfree.life@gmail.com>', // sender address
         to: user.email, // list of receivers
-        subject: 'Hello ' + user.username, // Subject line
-        text: 'Hello ' + user.username, // plain text body
-        html: '<b>Hello ' + user.username + '</b>' // html body
+        subject: 'Hello ' + user.username + ', Welcome To Think Free!', // Subject line
+        text: body, // plain text body
+        html: '<p>' + body + '</p>' // html body
     };
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-           // return console.log(error);
+            return console.log(error);
         }
-        //console.log('Message sent: %s', info.messageId);
+        console.log('Message sent: %s', info.messageId);
         res.status(200).json({email: 'message sent: ' + info})
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
