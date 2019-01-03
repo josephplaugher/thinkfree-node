e125a98b6ed62f3b9faa0b38b3d3538fe0eca18c
Joseph <joseph@appreciateco.com>
2019-01-03 06:54:13 -0800

    digital ocean troubleshoot

--- src/server.js
+++ src/server.js
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   next();
 });
 
+
 app.use(bodyParser.urlencoded({ extended: false })); // Parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // Parse application/json data
 
